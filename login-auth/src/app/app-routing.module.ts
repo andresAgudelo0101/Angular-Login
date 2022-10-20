e440917f4149ca5fc5793b1c17a4c4a1f2c5626d
andres agudelo <echeverry11298@gmail.com>
2022-10-20 16:42:34 -0500

    initial frontend configuration

--- login-auth/src/app/app-routing.module.ts
+++ login-auth/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SecretComponent } from './components/secret/secret.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'',component:HomeComponent},
+  {path:'secret',component:SecretComponent},
+  {path:'**', pathMatch:'full', redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
